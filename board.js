b333b338e0c70d2cfea34942689f7790e5a08382
VasilNikolov <vasil97@yahoo.com>
2018-04-17 15:54:21 +0300

    initial board spec

---
+++ board.js
@@ -0,0 +1,29 @@
+/*
+    Project for bluetooth board case.
+    All dimensions are in mm, tolerance is 0.1mm.
+*/
+
+const board = {
+    dimensions: {
+        width: 61.7,
+        length: 58.3,
+        thickness: 1.6,
+        heightFromBoardTop: 12.5,
+        heightFromBoardBottom: 1.7,
+        microUsbGap: 7.5,
+        headphoneGap: 9.3,
+        holesDiameter: 2.5,
+        topRightHole: {
+            top: 15.4,
+            right: 2.5
+        },
+        topLeftHole: {
+            top: 2.5,
+            left: 3.4
+        },
+        bottomLeftHole: {
+            bottom: 2.5,
+            left: 3.6
+        }
+    }
+}
